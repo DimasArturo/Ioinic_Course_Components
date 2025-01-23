b2d22977e75fc6e148cea1267d4579598de40142
DimasArturo <montalcoarturo@gmail.com>
2025-01-22 19:47:12 -0500

    datetimes

--- src/app/pages/date-time/date-time.page.ts
+++ src/app/pages/date-time/date-time.page.ts
@@ -8,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DateTimePage implements OnInit {
 
+  // Fecha inicial para el selector
+  fechaNaci: Date = new Date();
+
+  // Valores personalizados para los años
+  customYearValues = [2025, 2020, 2016, 2008, 2004, 2000, 1996];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  // Función que se ejecuta al cambiar la fecha
+  cambioFecha(event: any) {
+    console.log(event);
+    console.log(new Date(event.detail.value));
+  }
+
+  // Función para el botón "Hola"
+  logHola() {
+    console.log('Hola');
+  }
+
+  // Función para el botón "Mundo"
+  logMundo() {
+    console.log('Mundo');
   }
 
 }
